4f341813caa27efcaa5e69a9a33a8bf0858ffae4
Mrutyunjaya Senapati <mrutyunjayasenapati007@gmail.com>
2024-10-17 13:20:50 +0530

    UI

---
+++ Oct 17/Calculate.js
@@ -0,0 +1,12 @@
+export function Add(a,b){
+    return a+b;
+}
+export function Sub(a,b){
+    return a-b;
+}
+export function MultiPlication(a,b){
+    return a*b;
+}
+export function Divison(a,b){
+    return a/b;
+}
---
+++ Oct 17/script.js
@@ -0,0 +1,9 @@
+export default  function Users(){
+    return "User Module";
+}
+export function Admin(){
+    return "Admin Module";
+}
+export function vendor(){
+    return "Vendor Module";
+}
